be32b1ed1264156b49f4e58310f12a10834541da
James Calverley <james.calverley3@gmail.com>
2020-03-27 13:54:22 -0400

    cleans up DiscoverPage code

--- src/components/DiscoverPage.js
+++ src/components/DiscoverPage.js
@@ -3,11 +3,8 @@ import PuppyBeFriend from './PuppyBeFriend'
 
 function DiscoverPage() {
   const discoverStyle = {
-    image: { },
+    image: {},
     alert: { display: 'none' },
-    imageWrapper: {margin: '0 auto', position: 'relative', width: '40vw'},
-    imageLike: { position: 'absolute', left: '20px', bottom: '50px', borderRadius: '50%'},
-    imageDislike: { position: 'absolute', right: '20px', bottom: '50px', borderRadius: '50%'}
   }
   return (
     <div>
@@ -15,11 +12,9 @@ function DiscoverPage() {
         <h1>Make New Friends</h1>
         <h2>Thumbs up on any pups you want to meet</h2>
         <PuppyBeFriend />
-      
         <h1>Make New Friends</h1>
-
         <div class="alert alert-success" role="alert" style={discoverStyle.alert}>
-        This is a success alert—check it out!
+          This is a success alert—check it out!
         </div>
 
       </div>
